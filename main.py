4d29fd853636e1913965101dc4470f32cc1852fc
Thappi <mohamed.thappi@accenture.com>
2025-07-04 23:22:53 +0530

    Let's fastAPI

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+@app.get("/")
+async def root():
+    return {"message": "Hello!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post.model_dump())
+    print(post.rating)
+    return {"data": post}
