7e685da6ea456360456d7dff5f9b7ff50abfa1ff
Thappi <mohamed.thappi@accenture.com>
2025-07-05 00:08:01 +0530

    CRUD!

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -11,16 +12,34 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "title of post 2", "content": "content of post 2", "id": 2}]
 @app.get("/")
 async def root():
     return {"message": "Hello!"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-    print(post.model_dump())
-    print(post.rating)
-    return {"data": post}
+    # print(post.model_dump())
+    # print(post.rating)
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+# @app.get("/posts/latest")
+# def get_latest_post():
+#     return {"data": my_posts[len(my_posts)-1]}
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    print(id)
+    for post in my_posts:
+        if post['id'] == id:
+            return {"data": post}
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'message': f"Post with id: {id} does not exist!"}
