4c07877d72e932a730b4e8567285f5f2e74e354e
Thappi <mohamed.thappi@accenture.com>
2025-07-05 21:22:06 +0530

    CRUD Done!

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ class Post(BaseModel):
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
             {"title": "title of post 2", "content": "content of post 2", "id": 2}]
+
+def find_post_ind(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello!"}
@@ -41,7 +48,28 @@ def get_post(id: int):
     for post in my_posts:
         if post['id'] == id:
             return {"data": post}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id: {id} does not exist!")
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'message': f"Post with id: {id} does not exist!"}
+
+@app.delete("/posts/{id}",status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    ind = find_post_ind(id)
+    if ind == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail = f"Post with id: {id} does not exist!")
+    my_posts.pop(ind)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    ind = find_post_ind(id)
+    if ind == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail = f"Post with id: {id} does not exist!")
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[ind] = post_dict
+    return{'data': post_dict}
+
